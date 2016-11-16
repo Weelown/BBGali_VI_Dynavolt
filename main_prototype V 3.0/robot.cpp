a5ed6979733f75936feb7e4b2509187ac202b614
Weelown <romain.gerardin@hotmail.fr>
2016-11-16 11:38:37 +0100

    Add files via upload
    Fonctions de déplacement V3.0

---
+++ main_prototype V 3.0/robot.cpp
@@ -0,0 +1,42 @@
+/*
+* @Author: Romain_G�rardin
+* @Date:   2016-11-15 15:38:19
+* @Last Modified by:   Romain_G�rardin
+* @Last Modified time: 2016-11-15 17:09:51
+*/
+
+
+#define pi 3.1415926536
+#define ABSCISSE_DEPART 0
+#define ORDONNE_DEPART 0
+#define RAYON_DEPART ((ABSCISSE_DEPART^2)+(ORDONNE_DEPART^2))^(0.5)
+
+#if defined ABSCISSE_DEPART == 0
+#define ANGLE_DEPART = pi / 2
+#else
+#define ANGLE_DEPART atan(ORDONNE_DEPART / ABSCISSE_DEPART)
+#endif
+
+// #include <iostream>
+#include <math.h>
+#include "robot.h"
+
+using namespace std;
+
+
+	Robot::Robot()
+	:m_x(ABSCISSE_DEPART),m_y(ORDONNE_DEPART),m_r(RAYON_DEPART),m_angle(ANGLE_DEPART),moteur_droit(Moteur("Moteur_droit", 0, "Arret")),Moteur("Moteur_gauche", 0, "Arret")
+	{}
+
+	void Robot::set_x(float _x) {m_x = _x;}
+	void Robot::set_y(float _y) {m_y = _y;}
+	void Robot::set_r(float _r) {m_r = _r;}
+	void Robot::set_angle(float _angle) {m_angle = _angle;}
+
+	float Robot::get_x() {return m_x;}
+	float Robot::get_y() {return m_y;}
+	float Robot::get_r() {return m_r;}
+
+	float Robot::get_angle() {return m_angle;}
+	Moteur Robot::get_moteur_droit() {return m_moteur_droit;}
+	Moteur Robot::get_moteur_gauche() {return m_moteur_gauche;}
---
+++ main_prototype V 3.0/robot.h
@@ -0,0 +1,32 @@
+#ifndef robot_h
+#define robot_h
+
+#include "moteur.h"
+
+class Robot
+{
+private:
+	float m_x;
+	float m_y;
+	float m_r;
+	float m_angle;
+	Moteur m_moteur_droit;
+	Moteur m_moteur_gauche;
+
+public:
+	Robot();
+
+	void set_x(float _x);
+	void set_y(float _y);
+	void set_r(float _r);
+	void set_angle(float _angle);
+
+	float get_x();
+	float get_y();
+	float get_r();
+	float get_angle();
+	Moteur get_moteur_droit();
+	Moteur get_moteur_gauche();
+};
+
+#endif
